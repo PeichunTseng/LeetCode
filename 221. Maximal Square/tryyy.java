1e87df028a8c73fe2d170cd77670d9222c5d9bfa
PeiChun Tseng <p81815@hotmail.com>
2020-04-27 15:01:10 -0400

    2020-4-27

--- 221. Maximal Square/tryyy.java
+++ 221. Maximal Square/tryyy.java
@@ -6,62 +6,53 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.HashMap;
 import java.util.Map;
-//import java.util.Entry;
-//import java.util.*;
-
-
 
 public class tryyy
 {
 ////////////Time complexity : O(mn) //////////////////////
 ////////////Space complexity : O(mn)//////////////////////
 
-
-   // public int maximalSquare(char[][] matrix) {
-   //  int rows = matrix.length;
-   //  int cols = rows > 0 ? matrix[0].length : 0;
-   //      int[][] dp = new int[rows + 1][cols + 1];
-   //      int maxsqlen = 0;
-   //      for (int i = 1; i <= rows; i++) {
-   //          for (int j = 1; j <= cols; j++) {
-   //              if (matrix[i-1][j-1] == '1'){
-   //                  dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
-   //                  //System.out.println(dp[i][j]);
-   //                  maxsqlen = Math.max(maxsqlen, dp[i][j]);
-   //              }
-   //          }
-   //      }
-   //      return maxsqlen * maxsqlen;
-        
-   //  }
-
+  // public int maximalSquare(char[][] matrix){
+  //   int row = matrix.length;
+  //   int col = row==0 ? 0 : matrix[0].length;
+  //   int[][] dp = new int[row+1][col+1];
+  //   int maxLen=0;
+  //   for(int i=1;i<row+1;i++){
+  //     for(int j=1;j<col+1;j++){
+  //       if(matrix[i-1][j-1]=='1'){
+  //         dp[i][j]=Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
+  //         maxLen=Math.max(maxLen,dp[i][j]);
+  //       }
+  //     }
+  //   }
+  //   return maxLen*maxLen;
+  // }
+  
 
 ////////////Time complexity : O(mn) //////////////////////
 ////////////Space complexity : O(n)//////////////////////
 
-    public int maximalSquare(char[][] matrix) {
-        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
-        int[] dp = new int[cols + 1];
-        int maxsqlen = 0, prev = 0;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                int temp = dp[j];
-                if (matrix[i - 1][j - 1] == '1') {
-                    dp[j] = Math.min(Math.min(dp[j - 1], prev), dp[j]) + 1;
-                    maxsqlen = Math.max(maxsqlen, dp[j]);
-                } else {
-                    dp[j] = 0;
-                }
-                prev = temp;
-            }
+    public int maximalSquare(char[][] matrix){
+      int row = matrix.length;
+      int col = row==0 ? 0 : matrix[0].length;
+      int[] dp = new int[col+1];
+      int maxLen=0;
+      for(int i=1;i<row+1;i++){
+        int pre=0;
+        for(int j=1;j<col+1;j++){
+          int temp=dp[j];
+          if(matrix[i-1][j-1]=='1'){
+            dp[j]=Math.min(Math.min(dp[j-1],dp[j]),pre)+1;
+            maxLen=Math.max(maxLen,dp[j]);
+          }
+          else
+            dp[j]=0;
+          pre=temp;
         }
-        return maxsqlen * maxsqlen;
+      }
+      return maxLen*maxLen;
     }
 
-
-
-  
-   
 
     public static void main(String[] args) 
     {
@@ -73,8 +64,6 @@ public class tryyy
        int a=tr.maximalSquare(grid);
        System.out.println(a);
     }
-
-
 
 }
 
