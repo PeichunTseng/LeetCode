134605839a4d73b1b7220cdd9db0ee1d09a0f0bc
PeiChun Tseng <p81815@hotmail.com>
2020-05-08 09:42:57 -0400

    2020-5-8

--- 53. Maximum Subarray/tryyy.java
+++ 53. Maximum Subarray/tryyy.java
@@ -6,10 +6,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.HashMap;
 import java.util.Map;
-//import java.util.Entry;
-//import java.util.*;
-
-
 
 public class tryyy
 {
@@ -23,38 +19,54 @@ public class tryyy
       return res;
     }
 
-
-    public static void main(String[] args){
-       tryyy tr=new tryyy();
-       int[] a={-2,1,-3,4,-1,6,-1,10};//18
-       int b=tr.maxSubArray(a);
-       System.out.println(b);        
+    /////////////////////////return index//////////////////////
+    public int[] maxSubArray(int[] nums){
+       int res=nums[0];
+       int curr=nums[0];
+       int start=0;
+       int end=0;
+       if(nums.length==1)
+           return new int[] {curr,start,end};      
+       for(int i=1;i<nums.length;i++){
+         if(nums[i]>curr+nums[i]){
+           start=i;
+         }
+         curr=Math.max(curr+nums[i],nums[i]);
+         if(curr>res)
+           end=i;
+         res=Math.max(res,curr);
+       }
+       return new int[]{res,start,end};
     }
 
-    /////////////////////////返回index//////////////////////
-        //  public int[] maxSubArray(int[] nums){
-        //     int max=nums[0];
-        //     int currentvalue=nums[0];
-        //     int begin=0;
-        //     int end=0;
-        //     if(nums.length==1)
-        //         return new int[] {currentvalue,begin,end};      
-        //     for(int i=1;i<nums.length;i++){
-        //         if(nums[i]>currentvalue+nums[i])
-        //           begin=i;
-        //         currentvalue=Math.max(nums[i],currentvalue+nums[i]);
-        //         if(currentvalue>=max)
-        //           end=i;
-        //         max=Math.max(max,currentvalue);
-        //     }
-        //     return Arrays.copyOfRange(nums,begin,end+1);;
-        // }
+    ///////////////////at lease length>=k//////////////////////
+    public int[] maxSubArray(int[] nums, int k){
+      int res=Integer.MIN_VALUE;
+      int curr=0;
+      int start=0;
+      int end=0;
+      for(int i=0;i<nums.length;i++){
+        curr+=nums[i];
+        if(i>=k-1 && curr>res){
+          res=curr;
+          end=i;
+        }
+        if(i-start>=k-1){
+          curr-=nums[start];
+          start++;
+        }
+      }
+      return new int[]{res,end-k+1,end};
+    }
 
-        // public static void main(String[] args){
-        //    tryyy tr=new tryyy();
-        //    int[] a={-2,1,-3,4,-1,6,-1,10};
-        //    int[] ans=tr.maxSubArray(a);
-        //    for(int b:ans)
-        //    System.out.println(b+"  ");
-        // }
+    public static void main(String[] args){
+      tryyy tr=new tryyy();
+      int[] a={-2,1,-3,4,-1,6,-1,10};
+      // int[] ans=tr.maxSubArray(a);
+      // for(int b:ans)
+      //   System.out.println(b+"  ");
+      int[] ans=tr.maxSubArray(a,3);
+      for(int b:ans)
+        System.out.println(b+"  ");
+    }
 }
