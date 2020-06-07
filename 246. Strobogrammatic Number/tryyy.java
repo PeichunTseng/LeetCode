3a9ff225bbc04ddda296a33f9cf700bc3344c52d
PeiChun Tseng <p81815@hotmail.com>
2020-06-06 21:47:42 -0400

    2020-6-6

--- 246. Strobogrammatic Number/tryyy.java
+++ 246. Strobogrammatic Number/tryyy.java
@@ -15,25 +15,45 @@ import java.util.LinkedList;
 public class tryyy
 {
 /////////////////////////Mine///////////////////////
+    // public boolean isStrobogrammatic(String num) {
+    //     int index=0;
+    //     char[] end=new char[num.length()];
+    //     for(int i=num.length()-1;i>=0;i--){
+    //         char ch=num.charAt(i);
+    //         if(ch=='0')
+    //             end[index++]='0';
+    //         else if(ch=='1')
+    //             end[index++]='1';
+    //         else if(ch=='6')
+    //             end[index++]='9';
+    //         else if(ch=='8')
+    //             end[index++]='8';
+    //         else if(ch=='9')
+    //             end[index++]='6';
+    //         else 
+    //             return false;
+    //     }
+    //     return num.equals(String.valueOf(end));
+    // }
+
+/////////////////////////Mine Improve///////////////////////
     public boolean isStrobogrammatic(String num) {
         int index=0;
-        char[] end=new char[num.length()];
         for(int i=num.length()-1;i>=0;i--){
             char ch=num.charAt(i);
-            if(ch=='0')
-                end[index++]='0';
-            else if(ch=='1')
-                end[index++]='1';
-            else if(ch=='6')
-                end[index++]='9';
-            else if(ch=='8')
-                end[index++]='8';
-            else if(ch=='9')
-                end[index++]='6';
-            else 
-                return false;
+            if(ch=='0' && num.charAt(index++)=='0')
+                continue;
+            else if(ch=='1' && num.charAt(index++)=='1')
+                continue;
+            else if(ch=='6' && num.charAt(index++)=='9')
+                continue;
+            else if(ch=='8'&& num.charAt(index++)=='8')
+                continue;
+            else if(ch=='9'&& num.charAt(index++)=='6')
+                continue;
+            return false;
         }
-        return num.equals(String.valueOf(end));
+        return true;
     }
 
     public static void main(String[] args){
