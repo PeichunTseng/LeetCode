eff987e48304d69f164b97e3546f2db2d2fbd60b
PeiChun Tseng <p81815@hotmail.com>
2020-07-06 09:06:56 -0400

    2020-7-6

--- 66. Plus One/tryyy.java
+++ 66. Plus One/tryyy.java
@@ -8,65 +8,52 @@ import java.util.HashMap;
 import java.util.Map;
 import java.lang.Math; 
 
-
 public class tryyy
 {
-
-  ///////////////////mine ///////////////////////////////////////
+  ///////////////////mine///////////////////////////////////////
   // public int[] plusOne(int[] digits) {
-  //       int next=0;
-  //       Stack<Integer> s = new Stack<>();
-  //       for(int i=digits.length-1;i>=0;i--){
-  //         int tem = digits[i]+next;
-  //         if(i==digits.length-1)
-  //           tem++;
-  //         s.push(tem%10);
-  //         next=tem/10;
-  //       }
-  //       if(next!=0)
-  //         s.push(next);
-        
-  //       int[] res= new int[s.size()];
-  //       int i=0;
-  //       while(!s.isEmpty()){
-  //         res[i]=s.pop();
-  //           i++;
-  //       }
-  //       return res;   
+  //     int temp=1;
+  //     int[] ans=new int[digits.length+1];
+  //     for(int i=digits.length-1;i>=0;i--){
+  //         digits[i]+=temp;
+  //         temp=digits[i]/10;
+  //         digits[i]=digits[i]%10;
+  //     }
+  //     if(temp==0)
+  //         return digits;
+  //     ans[0]=temp;
+  //     for(int i=0;i<digits.length;i++)
+  //         ans[i+1]=digits[i];
+  //     return ans;
   // }
 
-  ///////////////////較好解 ///////////////////////////////////////
+  ///////////////////較好解///////////////////////////
   public int[] plusOne(int[] digits) {
-        
-       for(int i = digits.length - 1; i >= 0; i--) {
-            if(digits[i] != 9) {
-                digits[i]++;
-                return digits;
-            } else {
-                digits[i] = 0;
-            }
+     for(int i = digits.length - 1; i >= 0; i--) {
+        if(digits[i] != 9) {
+            digits[i]++;
+            return digits;
+        } 
+        else {
+            digits[i] = 0;
         }
-        
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
-        return result;
-    }
+      }
+      
+      int[] result = new int[digits.length + 1];
+      result[0] = 1;
+      return result;
+  }
 
 
-    public static void main(String[] args) 
-    {
-       tryyy tr = new tryyy();
-       // int[] a={9};
-       // int[] a={1,2,3};
-       int[] a={9,9};
-       int[] b=tr.plusOne(a);
-       for(int c:b)
-          System.out.println(c);
-
-    }
-
-
-
+  public static void main(String[] args){
+     tryyy tr = new tryyy();
+     // int[] a={9};
+     // int[] a={1,2,3};
+     int[] a={9,9};
+     int[] b=tr.plusOne(a);
+     for(int c:b)
+        System.out.println(c);
+  }
 }
 
 
