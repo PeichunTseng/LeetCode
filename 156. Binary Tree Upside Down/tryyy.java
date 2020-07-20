1e4bd4c2f7cdea7a64e69766063246f806367974
PeiChun Tseng <p81815@hotmail.com>
2020-07-20 14:23:49 -0400

    2020-7-20

---
+++ 156. Binary Tree Upside Down/tryyy.java
@@ -0,0 +1,106 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+import java.util.PriorityQueue; 
+import java.util.LinkedList;
+
+//binary tree//
+public class tryyy
+{
+    private TreeNode  root;
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) {
+            val = x;
+        }
+
+    }
+
+    public static TreeNode createTree(Integer[] array) {
+        if (array == null || array.length==0) {
+            return null;
+        }
+
+        Queue<TreeNode> treeNodeQueue = new LinkedList<>();
+        Queue<Integer> integerQueue = new LinkedList<>();
+        for (int i = 1; i < array.length; i++) {
+            integerQueue.offer(array[i]);
+        }
+
+        TreeNode treeNode = new TreeNode(array[0]);
+        treeNodeQueue.offer(treeNode);
+
+        while (!integerQueue.isEmpty()){
+            Integer leftVal = integerQueue.isEmpty() ? null : integerQueue.poll();
+            Integer rightVal = integerQueue.isEmpty() ? null : integerQueue.poll();
+            TreeNode current = treeNodeQueue.poll();
+            if (leftVal !=null) {
+                    TreeNode left = new TreeNode(leftVal);
+                    current.left = left;
+                    treeNodeQueue.offer(left);
+            }
+            if (rightVal !=null){
+                    TreeNode right = new TreeNode(rightVal);
+                    current.right = right;
+                    treeNodeQueue.offer(right);
+            }
+        }
+        return treeNode;
+    }
+
+    public List <Integer> inorderTraversal(TreeNode root) {
+        List <Integer> res = new ArrayList <>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List < Integer > res) {
+        if (root != null){
+          if (root.left != null){             
+            helper(root.left, res);
+          }
+          res.add(root.val);
+          if (root.right != null){
+            helper(root.right, res);
+          }
+        }
+    }
+//////////////////////////Others////////////////////////////////
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        TreeNode curr=root;
+        TreeNode next=null;
+        TreeNode pre=null;
+        TreeNode temp=null;
+
+        while(curr!=null){
+          next=curr.left;
+          curr.left=temp;
+          temp=curr.right;
+          curr.right=pre;
+
+          pre=curr;
+          curr=next;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args){
+       tryyy tr=new tryyy(); 
+       Integer[] input={1,2,3,4,5};
+       tr.root=tr.createTree(input);
+
+       TreeNode ans=tr.upsideDownBinaryTree(tr.root);
+       List<Integer> list=tr.inorderTraversal(ans);
+       System.out.println(list);           
+    }
+}
