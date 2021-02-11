13d04f2ac392e5f8645ca4f3266ecbe2c6193f51
PeiChun Tseng <p81815@hotmail.com>
2021-02-11 11:11:34 -0500

    2021-2-11

--- 332. Reconstruct Itinerary/tryyy.java
+++ 332. Reconstruct Itinerary/tryyy.java
@@ -39,6 +39,41 @@ public class tryyy
     // route JFK KUL
     //Ans [JFK, NRT, JFK, KUL]
 
+////////////////////Mine 20210211///////////////////////
+    public List<String> findItinerary(List<List<String>> tickets) {
+        int total = tickets.size()+1;
+        Map<String,List<String>> map = new HashMap<>();
+        for(List<String> ticket : tickets){
+            List<String> list = map.getOrDefault(ticket.get(0),new ArrayList<>());
+            list.add(ticket.get(1));
+            map.put(ticket.get(0),list);
+        }
+        for(String key:map.keySet()){
+            Collections.sort(map.get(key));
+        }
+        List<String> res = new ArrayList<>();
+        res.add("JFK");
+        help("JFK",map,res,total);
+        return res;
+    }
+    
+    public void help(String start, Map<String,List<String>> map, List<String> res, int total){
+        if(!map.containsKey(start) || map.get(start).size()==0){
+            return;
+        }
+        List<String> list = map.get(start);
+        int time = list.size();
+        for(int i=0;i<time;i++){
+            String next=list.remove(0);
+            res.add(next);
+            help(next,map,res,total);
+            if(res.size()==total)
+                return;
+            list.add(next);
+            res.remove(res.size()-1);
+        }
+    }
+
     public static void main(String[] args){
        tryyy tr=new tryyy(); 
        String[][] arr={{"JFK","KUL"},{"JFK","NRT"},{"NRT","JFK"}};
