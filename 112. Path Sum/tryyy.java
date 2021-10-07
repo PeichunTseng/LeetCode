b732a9a1c9517daa22dd9059f8c251c5d551ed54
PeiChun Tseng <p81815@hotmail.com>
2021-10-06 22:03:49 -0500

    2021-10-6

---
+++ 112. Path Sum/tryyy.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+import java.util.LinkedList;
+
+//binary tree//
+public class tryyy{
+    private TreeNode  root;
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) {
+            val = x;
+        }
+
+    }
+    public static TreeNode createTree(Integer[] array) {
+        if (array == null || array.length==0) {
+            return null;
+        }
+
+        Queue<TreeNode> treeNodeQueue = new LinkedList<>();
+        Queue<Integer> integerQueue = new LinkedList<>();
+        for (int i = 1; i < array.length; i++) {
+            integerQueue.offer(array[i]);
+        }
+
+        TreeNode treeNode = new TreeNode(array[0]);
+        treeNodeQueue.offer(treeNode);
+
+        while (!integerQueue.isEmpty()){
+            Integer leftVal = integerQueue.isEmpty() ? null : integerQueue.poll();
+            Integer rightVal = integerQueue.isEmpty() ? null : integerQueue.poll();
+            TreeNode current = treeNodeQueue.poll();
+            if (leftVal !=null) {
+                    TreeNode left = new TreeNode(leftVal);
+                    current.left = left;
+                    treeNodeQueue.offer(left);
+            }
+            if (rightVal !=null){
+                    TreeNode right = new TreeNode(rightVal);
+                    current.right = right;
+                    treeNodeQueue.offer(right);
+            }
+        }
+        return treeNode;
+    }
+///////////////////////////mine/////////////////////////////
+    public boolean hasPathSum(TreeNode root, int sum) {
+      if(root == null) return false;
+      if(root.left == null && root.right == null) return sum-root.val == 0;
+      return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+    }
+
+
+    public static void main(String[] args) {
+       tryyy tr=new tryyy(); 
+       Integer[] input={5,4,8,11,null,13,4,7,2,null,null,null,1};
+       tr.root=tr.createTree(input);
+       System.out.println(tr.hasPathSum(tr.root,22));
+    }
+}
